71d4063c39cd028c17defee3c330746ff360ae60
harsh kapoor <harshkapoor458@gmail.com>
2024-02-11 21:09:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 1001;
+const path=require("path")
+app.listen(port, () => {
+    console.log("server is created at",port)
+})
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname,"public")))
+
+
+app.get("/", (req, res) => {
+        console.log("/")
+        res.render("home.ejs");
+})
+app.get("/contact", (req, res) => {
+    console.log("contact");
+    res.render("contact.ejs")
+})
+app.get("/about", (req, res) => {
+    console.log("about")
+    res.render("about.ejs")
+})
+app.get("/works", (req, res) => {
+    console.log("work")
+    res.render("work.ejs")
+})
+app.get("/services", (req, res) => {
+    console.log("product")
+    res.render("services.ejs")
+})
+app.get("*", (req, res) => {
+    console.log("*")
+    res.send("page not found")
+})
+    
